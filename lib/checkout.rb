fd78a52f66b5d2c1a026d2c8974ee3e7337c275e
Abby Hudson <abbyeleanorhudson@gmail.com>
2021-09-24 18:31:31 +0200

    calculate prices for the basket using the discounts hash

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -18,24 +18,37 @@ class Checkout
     total = 0
     basket.each do |item, count|
       p basket
-      if item == :apple || item == :pear
-        if (count % 2 == 0)
-          total += prices.fetch(item) * (count / 2)
-        else
-          total += prices.fetch(item) * count
-        end
-      elsif item == :banana || item == :pineapple
-        if item == :pineapple
-          total += (prices.fetch(item) / 2)
-          total += (prices.fetch(item)) * (count - 1)
-        else
-          total += (prices.fetch(item) / 2) * count
-        end
+      p count
+      if count % Discount::DISCOUNTS[item.to_sym][:minimum_quantity] == 0
+        split_count = count.divmod(Discount::DISCOUNTS[item.to_sym][:minimum_quantity])
+        p split_count
+        discounted_quantity = split_count[0]
+        total += prices.fetch(item) * Discount::DISCOUNTS[item.to_sym][:discount_quantity] * discounted_quantity
+        total += prices.fetch(item) * split_count[1]
       else
         total += prices.fetch(item) * count
       end
-    end
 
+      # if item == :apple || item == :pear
+      #   if (count % 2 == 0)
+      #     total += prices.fetch(item) * (count / 2)
+      #   else
+      #     total += prices.fetch(item) * count
+      #   end
+      # elsif item == :banana || item == :pineapple
+      #   if item == :pineapple
+      #     total += (prices.fetch(item) / 2)
+      #     total += (prices.fetch(item)) * (count - 1)
+      #   else
+      #     total += (prices.fetch(item) / 2) * count
+      #   end
+      # else
+      #   total += prices.fetch(item) * count
+      # end
+
+      #note: make sure if you buy 6 then the discount is also applied
+    end
+    p total
     total
   end
 
@@ -48,10 +61,11 @@ end
 
 class Discount
   DISCOUNTS = {
-    "apple": { "minimum_quantity": 2, "discount_quantity": 1, "maximum_quantity": nil},
-    "pear": { "minimum_quantity": 2, "discount_quantity": 1, "maximum_quantity": nil},
-    "banana": { "minimum_quantity": 1, "discount_quantity": 0.5, "maximum_quantity": nil},
+    "apple": { "minimum_quantity": 2, "discount_quantity": 1, "maximum_quantity": 100},
+    "orange": { "minimum_quantity": 1, "discount_quantity": 1, "maximum_quantity": 100},
+    "pear": { "minimum_quantity": 2, "discount_quantity": 1, "maximum_quantity": 100},
+    "banana": { "minimum_quantity": 1, "discount_quantity": 0.5, "maximum_quantity": 100},
     "pineapple": { "minimum_quantity": 1, "discount_quantity": 0.5, "maximum_quantity": 1},
-    "mango": { "minimum_quantity": 4, "discount_quantity": 3, "maximum_quantity": nil}
+    "mango": { "minimum_quantity": 4, "discount_quantity": 3, "maximum_quantity": 100}
   }
 end
